407eb7fe7057e8aa9d449a18f25b177fd428c5ff
Akejyo <987383393@qq.com>
2023-10-24 18:07:59 +0800

    fix lqip bugs

--- _javascript/modules/components/img-loading.js
+++ _javascript/modules/components/img-loading.js
@@ -6,56 +6,56 @@ const ATTR_DATA_SRC = 'data-src';
 const ATTR_DATA_LQIP = 'data-lqip';
 
 const cover = {
-  SHIMMER: 'shimmer',
-  BLUR: 'blur'
+    SHIMMER: 'shimmer',
+    BLUR: 'blur'
 };
 
 function removeCover(clzss) {
-  $(this).parent().removeClass(clzss);
+    $(this).parent().removeClass(clzss);
 }
 
 function handleImage() {
-  if (!this.complete) {
-    return;
-  }
+    if (!this.complete) {
+        return;
+    }
 
-  if (this.hasAttribute(ATTR_DATA_LQIP)) {
-    removeCover.call(this, cover.BLUR);
-  } else {
-    removeCover.call(this, cover.SHIMMER);
-  }
+    if (this.hasAttribute(ATTR_DATA_LQIP)) {
+        removeCover.call(this, cover.BLUR);
+    } else {
+        removeCover.call(this, cover.SHIMMER);
+    }
 }
 
 /**
  * Switches the LQIP with the real image URL.
  */
 function switchLQIP() {
-  const $img = $(this);
-  const src = $img.attr(ATTR_DATA_SRC);
+    const $img = $(this);
+    const src = $img.attr(ATTR_DATA_SRC);
 
-  $img.attr('src', encodeURI(src));
-  $img.removeAttr(ATTR_DATA_SRC);
+    $img.attr('src', encodeURI(src));
+    $img.removeAttr(ATTR_DATA_SRC);
 }
 
 export function loadImg() {
-  const $images = $('article img');
+    const $images = $('article img');
 
-  if ($images.length) {
-    $images.on('load', handleImage);
-  }
-
-  // Images loaded from the browser cache do not trigger the 'load' event
-  $('article img[loading="lazy"]').each(function () {
-    if (this.complete) {
-      removeCover.call(this, cover.SHIMMER);
+    if ($images.length) {
+        $images.on('load', handleImage);
     }
-  });
 
-  // LQIPs set by the data URI or WebP will not trigger the 'load' event,
-  // so manually convert the URI to the URL of a high-resolution image.
-  const $lqips = $(`article img[${ATTR_DATA_LQIP}="true"]`);
+    // Images loaded from the browser cache do not trigger the 'load' event
+    $('article img[loading="lazy"]').each(function() {
+        if (this.complete) {
+            removeCover.call(this, cover.SHIMMER);
+        }
+    });
 
-  if ($lqips.length) {
-    $lqips.each(switchLQIP);
-  }
+    // LQIPs set by the data URI or WebP will not trigger the 'load' event,
+    // so manually convert the URI to the URL of a high-resolution image.
+    const $lqips = $(`article img[${ATTR_DATA_LQIP}="true"]`);
+
+    if ($lqips.length) {
+        $lqips.each(switchLQIP);
+    }
 }
